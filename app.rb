851110cfcf56b10fee0ea443700d2ccf47fd2f16
Uma Manandhar <um_2003_8@hotmail.com>
2020-02-18 03:26:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ class App < Sinatra::Base
 
   get "/multiply/:num1/:num2" do
     @num_1 = params[:num1].to_i
-    @num_2 = params[:num2]
+    @num_2 = params[:num2].to_i
     binding.pry
     "#{@num_1.to_i} * #{@num_2.to_i}"
   end
